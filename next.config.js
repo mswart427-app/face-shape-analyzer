4d3f328d17850c6b4731b3d7f493df7c086165b9
mswart427-app <michael@myhairmail.com>
2024-12-07 11:04:10 -0600

    Add middleware for CORS and frame permissions

--- next.config.js
+++ next.config.js
@@ -9,35 +9,6 @@ const nextConfig = {
         pathname: '/cdn/shop/**',
       }
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*'
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, OPTIONS'
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-Requested-With, Content-Type, Accept'
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src * 'unsafe-inline' 'unsafe-eval'; frame-ancestors *; img-src * data:; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src *;"
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
-          }
-        ]
-      }
-    ]
   }
 }
 
