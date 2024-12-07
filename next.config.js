c26fe313bb253d961e0a0efbe5870eb2b413d746
mswart427-app <michael@myhairmail.com>
2024-12-07 10:28:35 -0600

    Update security headers to allow Shopify embedding

--- next.config.js
+++ next.config.js
@@ -17,11 +17,19 @@ const nextConfig = {
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: '*'  // Allow embedding from any domain for now
+            value: '*'
           },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; frame-ancestors 'self' https://*.myshopify.com https://*.myhairmail.com https://myhairmail.com; img-src 'self' https: data:; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; connect-src 'self' https://*;"
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL'
           }
         ]
       }
