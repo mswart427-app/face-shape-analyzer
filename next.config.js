66dc2a9ee4289472ffcc854d9004102529177b3d
mswart427-app <michael@myhairmail.com>
2024-12-12 20:01:33 -0600

    Update CSP to allow images from cdn.shopify.com

--- next.config.js
+++ next.config.js
@@ -7,9 +7,28 @@ const nextConfig = {
         hostname: 'myhairmail.com',
         port: '',
         pathname: '/cdn/shop/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+        port: '',
+        pathname: '/s/files/**',
       }
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; img-src 'self' data: https://myhairmail.com https://cdn.shopify.com blob:; script-src 'self' 'unsafe-eval' 'unsafe-inline';"
+          }
+        ],
+      },
+    ]
   }
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
